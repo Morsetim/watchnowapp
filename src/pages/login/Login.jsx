506a3da072bcd4b2452f0beb8cf0288c69140fb8
Morsetim <mauricium.maurice@yahoo.com>
2022-03-14 08:49:59 +0100

    commit

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,50 @@
+import { useContext, useState } from "react";
+import { login } from "../../authContext/apiCalls";
+import { AuthContext } from "../../authContext/AuthContext";
+import logoImage from "../../images/istockphoto-1322037170-170667a-removebg-preview.png";
+import "./login.scss";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { dispatch } = useContext(AuthContext);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    login({ email, password }, dispatch);
+  };
+  return (
+    <div className="login">
+      <div className="top">
+        <div className="wrapper">
+          <img
+            className="logo"
+            src={logoImage}
+            alt="logo"
+          />
+        </div>
+      </div>
+      <div className="container">
+        <form>
+          <h1>Sign In</h1>
+          <input
+            type="email"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="loginButton" onClick={handleLogin}>
+            Sign In
+          </button>
+          <span>
+            New to Watchnow? <b>Sign up now.</b>
+          </span>
+        </form>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,74 @@
+import { useRef } from "react";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+import "./register.scss";
+import logoImage from "../../images/istockphoto-1322037170-170667a-removebg-preview.png";
+
+
+export default function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const history = useHistory();
+
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const usernameRef = useRef();
+
+  const handleStart = () => {
+    setEmail(emailRef.current.value);
+  };
+
+  const handleLogin = () => {
+    history.push("/login")
+  }
+  
+  const handleFinish = async (e) => {
+    e.preventDefault();
+    setPassword(passwordRef.current.value);
+    setUsername(usernameRef.current.value);
+    try {
+      await axios.post("https://watchnowapp.herokuapp.com/api/auth/register", { email,username, password });
+      history.push("/login");
+    } catch (err) {}
+  };
+
+  return (
+    <div className="register">
+      <div className="top">
+        <div className="wrapper">
+          <img
+            className="logo"
+            src={logoImage}
+            alt="logo"
+          />
+          <button className="loginButton" onClick={handleLogin}>Sign In</button>
+        </div>
+      </div>
+      <div className="container">
+        <h1>Watchnow more entertainment...</h1>
+        <h2>Watch anywhere. Watch anytime.</h2>
+        <p>
+          Ready to watch? Enter your email to create or restart your membership.
+        </p>
+        {!email ? (
+          <div className="input">
+            <input type="email" placeholder="email address" ref={emailRef} />
+            <button className="registerButton" onClick={handleStart}>
+              Get Started
+            </button>
+          </div>
+        ) : (
+          <form className="input">
+            <input type="username" placeholder="username" ref={usernameRef} />
+            <input type="password" placeholder="password" ref={passwordRef} />
+            <button className="registerButton" onClick={handleFinish}>
+              Start
+            </button>
+          </form>
+        )}
+      </div>
+    </div>
+  );
+}
