a914f0698e0ade3842128693980715ab8134b69f
Morsetim <mauricium.maurice@yahoo.com>
2022-03-16 11:43:02 +0100

    proxy1

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -16,7 +16,7 @@ export default function ListItem({ index, item }) {
   useEffect(() => {
     const getMovie = async () => {
       try {
-        const res = await axios.get("movies/find/" + item, {
+        const res = await axios.get("api/movies/find/" + item, {
           headers: {
             token:
             "Bearer "+JSON.parse(localStorage.getItem("user")).accessToken,
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -13,7 +13,7 @@ const Home = ({ type }) => {
     const getRandomLists = async () => {
       try {
         const res = await axios.get(
-          `lists${type ? "?type=" + type : ""}${
+          `api/lists${type ? "?type=" + type : ""}${
             genre ? "&genre=" + genre : ""
           }`,
           {
