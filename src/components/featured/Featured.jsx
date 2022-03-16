93be81e825f478f84e0922809889cd2a8ade3a29
Morsetim <mauricium.maurice@yahoo.com>
2022-03-16 12:10:00 +0100

    proxy3

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -8,7 +8,7 @@ export default function Featured({ type, setGenre }) {
   useEffect(() => {
     const getRandomContent = async () => {
       try {
-        const res = await axios.get(`api/movies/random?type=${type}`, {
+        const res = await axios.get(`movies/random?type=${type}`, {
           headers: {
             token:
               "Bearer "+JSON.parse(localStorage.getItem("user")).accessToken,
