26b72a428f7ce86914d675eeb08435bfe23208c7
Mauro <mauricium.maurice@yahoo.com>
2022-08-22 16:49:01 +0100

    netlify errors

--- src/redux/reducers/auth.js
+++ src/redux/reducers/auth.js
@@ -1,10 +1,7 @@
 import {
     USER_SIGNUP_REQUEST,
     USER_SIGNUP_SUCCESS,
-    USER_SIGNUP_FAILURE,
-    USER_SIGNIN_REQUEST,
-    USER_SIGNIN_SUCCESS,
-    USER_SIGNIN_FAILURE
+    USER_SIGNUP_FAILURE
 } from "../actionCreators"
 
 const initialState = {
